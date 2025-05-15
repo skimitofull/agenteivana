01582389477f50636e48ef9efd57e300cd6e2a02
skimitofull <skimitofull@gmail.com>
2025-05-15 14:02:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import base64
 
 # --- CONSTANTES GLOBALES ---
 LETTER_WIDTH, LETTER_HEIGHT = letter
-MARGIN = 33  # Pie de página de 33 pt
+MARGIN = 46  # Pie de página de 46 pt
 BASE_ROW_HEIGHT = 25
 HEADER_HEIGHT = 25
 LINE_SPACING = 12
